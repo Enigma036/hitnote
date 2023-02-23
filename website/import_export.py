8e3486368325d45cfb44c4d077b77549c2cce751
jablko <jablko3000@gmail.com>
2023-02-23 19:47:21 +0100

    Doplnění funkcionality import/export

--- website/import_export.py
+++ website/import_export.py
@@ -11,6 +11,7 @@ import os
 import csv
 import sqlite3
 import io
+import uuid
 
 
 
@@ -28,35 +29,68 @@ def page():
         
         # Když zmáčkne na tlačítko import
         if import_export_messsage == "IMPORT":
-            input = request.files["input_file"]
-            input.save(input.filename)
+            try:
+                notes = Note.query.filter_by(user_id=current_user.id).all()
+                for note in notes:
+                    db.session.delete(note)
+            except:
+                    error_messages = "Nepodařilo se smazat stávající záznamy"
+                    trida = "error"
 
-            with open(input.filename, 'r', newline="") as csv_file:
-                reader = csv.reader(csv_file)
+            try:
+                input = request.files["input_file"]
+                input.save(input.filename)
 
-                for row in reader:
-                    try:
-                        datum = datetime.strptime(row[1], '%Y-%m-%d').date()
-                        cas = row[2]
-                        jazyk = row[3]
-                        hodnoceni = row[4]
-                        text = row[5]
-                        new_note = Note(date = datum, language = jazyk, interval = cas, stars = hodnoceni, user_id = current_user.id, data = text)
-                        db.session.add(new_note)
-                        db.session.commit()
-                    except:
-                        error_messages = "Nepodařilo se importovat některé zprávy"
-                        trida = "error"
-            os.remove(input.filename)
-        
+                error_messages = "Soubor úspěšně importován"
+                trida = "success"
+
+                with open(input.filename, 'r', newline="") as csv_file:
+                    reader = csv.reader(csv_file)
+
+                    for row in reader:
+                        try:
+                            ajdy = row[0]
+                            datum = datetime.strptime(row[1], '%Y-%m-%d').date()
+                            cas = row[2]
+                            jazyk = row[3]
+                            hodnoceni = row[4]
+                            text = row[5]
+                            print(Note.query.filter_by(id=ajdy).all())
+                            if Note.query.filter_by(id=ajdy).all() == []:
+                                new_note = Note(id = ajdy, date = datum, language = jazyk, interval = cas, stars = hodnoceni, user_id = current_user.id, data = text)
+                            else:
+                                new_note = Note(id=str(uuid.uuid4()), date = datum, language = jazyk, interval = cas, stars = hodnoceni, user_id = current_user.id, data = text)
+                                error_messages = "Aby nedošlo ke kolizi, muselo být id alespoň jedné zprávy změněno"
+                                trida = "error"
+                            db.session.add(new_note)
+                            db.session.commit()
+                        except:
+                            error_messages += "\nNepodařilo se importovat některé zprávy"
+                            trida = "error"
+                os.remove(input.filename)
+            except:
+                error_messages = "Nepodařilo se nahrát soubor"
+                trida = "error"
+                try:
+                    for note in notes:
+                        db.session.add(note)
+                except:
+                    error_messages += "\nNepodařilo se nahrát zálohované zprávy"
+                    trida = "error"
         # Když zmáčkne na tlačítko export
         elif import_export_messsage == "EXPORT":
-            notes = Note.query.filter_by(user_id=current_user.id).all()
-            output = io.StringIO()
-            writer = csv.writer(output)
+            try:
+                notes = Note.query.filter_by(user_id=current_user.id).all()
+                output = io.StringIO()
+                writer = csv.writer(output)
 
-            for note in notes:
-                writer.writerow([note.id, note.date, note.interval, note.language, note.stars, note.data])
+                for note in notes:
+                    writer.writerow([note.id, note.date, note.interval, note.language, note.stars, note.data])
+                    error_messages = "Soubor úspěšně exportován"
+                    trida = "success"
+            except:
+                error_messages = "Nastala chyba při exportování souboru"
+                trida = "error"
 
             # Vytvoření odpovědi pro stahování souboru
             response = make_response(output.getvalue())
